9088fd7c4edcf9178a2f854e07e08c08300651f9
Menaka <menakakollu811@gmail.com>
2021-04-02 15:36:23 -0700

    Updated README. Few changes to code

--- CardGame_War.py
+++ CardGame_War.py
@@ -1,29 +1,54 @@
 import random
 class Card:
-    def __init__(self,rank):
+    def __init__(self,suit,rank):
         self.rank=rank
+        self.suit=suit
+
+    def get_rank(self):
+        return self.rank
+
+    def get_suit(self):
+        return self.suit
 
 class Deck:
     def __init__(self):
         self.cards=[]
 
     def generate_deck(self,suits,number_of_cardtypes):
-        for s in range(suits):
+        suits=['C','D','H','S'] # Clubs,Diamonds,Spades,Hearts
+        for s in suits:
             for cardtype in range(1,number_of_cardtypes+1):
-                self.cards.append(Card(cardtype))
+                self.cards.append(Card(s,cardtype))
         
     def shuffle_deck(self):
         random.shuffle(self.cards)
         return self.cards
 
     def split_deck(self,n):
+        # Can be extended for more players
         l=len(self.cards)//n
         return self.cards[:l],self.cards[l:]
-    
+
+class Player:
+    def __init__(self,name,cards,cards_on_table=[]):
+        self.name=name
+        self.cards=cards
+        self.cards_on_table=cards_on_table
+
+    def get_name(self):
+        return self.name
+
+    def get_cards(self):
+        return self.cards
+        
+    def get_cards_on_table(self):
+        return self.cards_on_table
+
+
 class Game:
     def __init__(self,player1_cards,player2_cards):
-        self.player1_cards=player1_cards
-        self.player2_cards=player2_cards
+        self.player1_cards=player1_cards 
+        self.player2_cards=player2_cards  
         self.player1_cards_ontable=[]
         self.player2_cards_ontable=[]
         self.round_number=0
@@ -35,7 +60,7 @@ class Game:
         self.min_card_count_war=3
 
     def compare_faceup_cards(self,rank1,rank2):
-        # 
+        # Can be extended for more players
         if rank1> rank2:
             return "player1"
         elif rank2> rank1:
@@ -56,47 +81,72 @@ class Game:
         elif len(cards)>self.min_card_count_war:
             return True
 
+    def is_end_of_game(self):
+
+        if self.round_number >1000:
+            print("Game is taking too long. Might as well Exit and start New game")
+            return True
+
+        if self.is_stack_empty(self.player1_cards):
+            print("Player2 is the Winner")
+            return True
+
+        elif self.is_stack_empty(self.player2_cards):
+            print("Player1 is the Winner")
+            return True
+
+        return False
+
+
     def play(self):
+        
         while True:
             self.round_number+=1
             while True:
-
                 if not self.is_stack_empty(self.player1_cards) or not self.is_stack_empty(self.player2_cards):
                     card_p1 =self.player1_cards.pop()
                     card_p2= self.player2_cards.pop()
-                    print("Current Player1 Faceup Card Value: ",card_p1.rank)
-                    print("Current PLayer2 Faceup Card Value: ",card_p2.rank)
-                    comp= self.compare_faceup_cards(card_p1.rank,card_p2.rank)
+                    rank1,suit1= card_p1.get_rank(),card_p1.get_suit()
+                    rank2,suit2=card_p2.get_rank(),card_p2.get_suit()
+                    print("Current Player1 Faceup Card Value: ",str(rank1)+ str(suit1))
+                    print("Current PLayer2 Faceup Card Value: ",str(rank2)+ str(suit2))
+                    comp= self.compare_faceup_cards(rank1,rank2)
 
-                if comp == "player1":
-                    self.player1_cards= [card_p1,card_p2]+ self.player1_cards_ontable+ self.player2_cards_ontable+ self.player1_cards
-                    self.player1_cards_ontable=[]
-                    self.player2_cards_ontable=[]
-                    break
-                elif comp == "player2":
-                    self.player2_cards=[card_p2,card_p1]+ self.player2_cards_ontable+ self.player1_cards_ontable+ self.player2_cards
-                    self.player1_cards_ontable=[]
-                    self.player2_cards_ontable=[]    
-                    break           
-                elif comp == "war":
-                    print("********* IN WAR **************")
-                    if not self.has_mincards(self.player1_cards):
-                        self.player1_cards_ontable.extend([card_p1]+self.player1_cards[:])
-                        self.player1_cards=[]
-                        self.player1_cards.append(self.player1_cards_ontable.pop())
-                    else:
-                        self.player1_cards_ontable.extend([card_p1]+self.player1_cards[-self.min_card_count_war:])
-                        self.player1_cards= self.player1_cards[:-self.min_card_count_war]
+                    if comp == "player1":
+                        self.player1_cards= [card_p1,card_p2]+ self.player1_cards_ontable+ self.player2_cards_ontable+ self.player1_cards # The order is not specified by the game rules
+                        self.player1_cards_ontable=[]
+                        self.player2_cards_ontable=[]
+                        break
+                    elif comp == "player2":
+                        self.player2_cards=[card_p2,card_p1]+ self.player2_cards_ontable+ self.player1_cards_ontable+ self.player2_cards # The order is not specified by the game rules
+                        self.player1_cards_ontable=[]
+                        self.player2_cards_ontable=[]    
+                        break           
+                    elif comp == "war":
+                        print("*********** IN WAR **************")
 
-                    if not self.has_mincards(self.player2_cards):
-                        self.player2_cards_ontable.extend([card_p2]+self.player2_cards[:])
-                        self.player2_cards=[]
-                        self.player2_cards.append(self.player2_cards_ontable.pop())
-                    else:
-                        self.player2_cards_ontable.extend([card_p2]+self.player2_cards[-self.min_card_count_war:])
-                        self.player2_cards= self.player2_cards[:-self.min_card_count_war]
+                        '''
+                        If the player does not have min cards to play WAR. All the player's cards on placed on table and
+                        the top card on the table is used to play the round.
+                        If the player runs out of card,the top card on the table is used to play the round
+                        '''
+                        if not self.has_mincards(self.player1_cards):
+                            self.player1_cards_ontable.extend([card_p1]+self.player1_cards[:])
+                            self.player1_cards=[]
+                            self.player1_cards.append(self.player1_cards_ontable.pop())
+                        else:
+                            self.player1_cards_ontable.extend([card_p1]+self.player1_cards[-3:]) 
+                            self.player1_cards= self.player1_cards[:-3]
 
-                    break
+                        if not self.has_mincards(self.player2_cards):
+                            self.player2_cards_ontable.extend([card_p2]+self.player2_cards[:])
+                            self.player2_cards=[]
+                            self.player2_cards.append(self.player2_cards_ontable.pop())
+                        else:
+                            self.player2_cards_ontable.extend([card_p2]+self.player2_cards[-3:])
+                            self.player2_cards= self.player2_cards[:-3]
+
+                        break
 
             print("After Round Number: ",self.round_number)
             print("Number of Cards for Player1: ", len(self.player1_cards))
@@ -104,22 +154,18 @@ class Game:
             print("Number of Cards for Player2: ", len(self.player2_cards))
             print("Number of Cards of Player2 on table: ", len(self.player2_cards_ontable))
             print("---------------------------------------------------")
-            if self.is_stack_empty(self.player1_cards):
-                print("Player2 is the Winner")
+
+            if self.is_end_of_game():
                 break
 
-            elif self.is_stack_empty(self.player2_cards):
-                print("Player1 is the Winner")
-                break
 
-               
 
 if __name__=="__main__":
     deck= Deck()
-    deck.generate_deck(4,13)
+    deck.generate_deck(4,13) # 52 Cards
     deck.shuffle_deck()
     cards_p1,cards_p2=deck.split_deck(2)
     game= Game(cards_p1,cards_p2)
     game.play()
-    # play game 
+
 
--- README.md
+++ README.md
@@ -9,10 +9,18 @@ Python implementation of card game (War)
 3. The cards won in the round are added to the bottom of deck of cards of the player that won the round
 4. WAR is when both players turn over cards of same rank
 5. Game continues till one player holds all the cards. That player is declared as winner.
+6. This code can not simulate exactly as the real life game , because it depends on the order in which user adds the cards won to the bottom of deck.It is not mentioned by game rules
 
 ## Corner Cases
 1. During WAR, each player has to place three cards off the top of their deck face down on the table. And fourth card is used to play the round.
 2. If WAR happens during WAR, need to repeat the process until there is a decisive victory
 3. If a player runs out of cards or does not have mininum number of cards to play in WAR. The player's last card is used to play the round.
+4. If the game is running for more than 1000 times, i am exiting the game to prevent from infinite loop
 
 ## If Given More time
+1. I have used List for peforming pop() and append() operation. This might cause an issue when the number of players grow and game should run on large scale. 
+With more time, I could like to implement using Queue data structure imported from Queue library.
+2. I have defined Player Class but I have not used it. If even more time , I would create each player using the Player class.
+3. Genelarizing the game for N players . Currently , only two players can play the game. I would like to extend the current functionality for N players. I have thought about the implementation.I would love to try this 
+4. My current code doesnt have any interaction with the user, becuase the game was running for many rounds . So, I did not implement interaction. But I would like to add that feature to this using cool animation.
+
